ea8862713ace6ce94431524265c65c0569441dcd
Camille Ingram <camille_d_ingram@live.com>
2025-04-18 13:53:08 -0400

    Complete Mod3 - Exercise 1 - looping exercises

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+
+        do {
+            System.out.print("I love Java");
+            count += 1;
+        } while (count < 6);
+    }
+}
